0ca49d8a8ad3691bd1c3c077a4bebf12fbcd7a9e
Greed7717 <greed170203@gmail.com>
2023-09-30 23:20:47 +0600

    'hw3'

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from aiogram import executor
 from config import dp
 from handlers import start, chat_actions, fsm_form, report
 from database import sql_commands
+from scraper import my_scraper, asyn_scraper
 
 
 async def onstart_up(_):
@@ -11,7 +12,9 @@ async def onstart_up(_):
 
 start.register_start_handlers(dp=dp)
 fsm_form.register_fsm_form_handlers(dp=dp)
-report.register_report_handler(dp=dp)
+# report.register_report_handler(dp=dp)
+my_scraper.register_scraper(dp=dp)
+# asyn_scraper.register_scraper(dp=dp)
 chat_actions.register_chat_actions_handlers(dp=dp)
 
 if __name__ == '__main__':
